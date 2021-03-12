20838fe3b1b9c9cda93fca85b8758510a708db53
frankokafor22 <okafor.e.frank@gmail.com>
2021-03-12 09:19:01 +0100

    update

--- src/main/java/com/cardverification/threelink/controller/CardDetailsController.java
+++ src/main/java/com/cardverification/threelink/controller/CardDetailsController.java
@@ -4,16 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cardverification.threelink.service.CardDetailsInfoService;
 
-@Controller
+@RestController
 @CrossOrigin(origins = "*")
 public class CardDetailsController {
 	@Autowired
@@ -21,13 +20,11 @@ public class CardDetailsController {
 	
 
 	@GetMapping(path = "/card-scheme/verify/{cardNumber}", produces = {MediaType.APPLICATION_JSON_VALUE })
-	@ResponseBody
 	public ResponseEntity getInfo(@PathVariable("cardNumber") String cardNumber) {
 		return new ResponseEntity<>(service.getCardInfo(cardNumber),HttpStatus.OK);
 	}
 
 	@GetMapping(path = "/card-scheme/stats", produces = {MediaType.APPLICATION_JSON_VALUE })
-	@ResponseBody
 	public ResponseEntity getAllDrinks(@RequestParam(value = "start", defaultValue = "0") int start,
 			@RequestParam(value = "limit", defaultValue = "25") int limit) {
 		return new ResponseEntity<>(service.getCardStats(start, limit), HttpStatus.OK);
